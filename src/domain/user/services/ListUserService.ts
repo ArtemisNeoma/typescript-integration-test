a852a8298eed0c1f5923cc989e68f28c462bd848
Neo-WRN <nicolas.destito@warren.com.br>
2022-08-22 10:01:59 -0300

    refactor: changed ListUserService failedError to readingError and fixed it's message

--- src/domain/user/services/ListUserService.ts
+++ src/domain/user/services/ListUserService.ts
@@ -4,14 +4,9 @@ import {
 } from 'interfaces/domain/services/service';
 import { IRepositoryUser } from 'interfaces/domain/repository';
 import { inject, injectable } from 'tsyringe';
-import StatusError from '@util/error';
-
 @injectable()
 export default class ListUserService implements IListUserService {
-  failedError: StatusError = new StatusError(
-    500,
-    'Error: Failed to readAll database',
-  );
+  readingError: Error = new Error('Failed to readAll database');
   constructor(
     @inject('UserRepository')
     private repository: IRepositoryUser,
@@ -23,7 +18,7 @@ export default class ListUserService implements IListUserService {
       const usersJSON = Object.fromEntries(allUsers);
       return usersJSON;
     } catch (err) {
-      throw this.failedError;
+      throw this.readingError;
     }
   }
 }
