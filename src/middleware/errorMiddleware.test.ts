aa9f116359405379a4a071bcf98bc1eafda8f01c
Neo-WRN <nicolas.destito@warren.com.br>
2022-08-26 15:53:43 -0300

    refactor: updated errorMiddleware to new changes

--- src/middleware/errorMiddleware.test.ts
+++ src/middleware/errorMiddleware.test.ts
@@ -4,8 +4,8 @@ import { errorMiddleware } from './errorMiddleware';
 
 const req = {} as Request;
 const res = {} as Response;
-res.status = jest.fn().mockImplementation(() => res);
-res.json = jest.fn().mockImplementation(() => res);
+res.status = jest.fn().mockReturnThis();
+res.json = jest.fn().mockReturnThis();
 const next = jest.fn() as NextFunction;
 const statusError = new StatusError(404, '');
 
@@ -13,6 +13,6 @@ describe('errorMiddleware', () => {
   it('Should send error as json with StatusError status code', () => {
     errorMiddleware(statusError, req, res, next);
     expect(res.status).toBeCalledWith(statusError.code);
-    expect(res.json).toBeCalledWith(statusError.message);
+    expect(res.json).toBeCalledWith({ error: statusError.message });
   });
 });
