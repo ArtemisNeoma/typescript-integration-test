cb54d11e2962903cb932c04dd7e592a4aaf4f74d
Neo-WRN <nicolas.destito@warren.com.br>
2022-08-26 11:25:57 -0300

    refactor: fixed names and moved mocks from UserRouter to separate file

--- src/presentation/routes/UserRouter.spec.ts
+++ src/presentation/routes/UserRouter.spec.ts
@@ -3,6 +3,14 @@ import app from '../../index';
 import UserRepository from '@domain/user/repository/UserRepository';
 import { IUser } from '@interfaces/domain/repository';
 import axios from 'axios';
+import {
+  mCpfEqualUser,
+  mCpfInvalidUser,
+  mCpfRepeatedUser,
+  mEmailRepeatedUser,
+  mockValidUser,
+  mPCodeInvalidUser,
+} from './mocks/UserRouter.mock';
 
 const spyRepository = {
   readAll: jest.spyOn(UserRepository.prototype, 'readAll'),
@@ -11,34 +19,20 @@ const spyRepository = {
 
 const expectedGetError = 'Error: Failed to readAll database';
 
-const mockUser: IUser = {
-  full_name: 'Teste do Teste',
-  email: 'test@test.com',
-  email_confirmation: 'test@test.com',
-  cpf: '123.456.789-09',
-  cellphone: '(47) 991234567',
-  birthdate: '2000-01-01',
-  email_sms: false,
-  whatsapp: false,
-  country: 'Brazil',
-  city: 'Blumenau',
-  postal_code: '89030-112',
-  address: 'Rua X, 001, Itoupava',
-};
 const expectedUser: IUser = {
-  ...mockUser,
+  ...mockValidUser,
   cpf: '12345678909',
   cellphone: '47991234567',
   birthdate: '2000-01-01T00:00:00.000Z',
-  postal_code: '89030112',
+  postal_code: '89010203',
 };
 
-const expectedDatabase = {
-  0: { email: expectedUser.email, cpf: expectedUser.cpf },
+const expectedListJson = {
+  0: { email: mEmailRepeatedUser.email, cpf: mCpfRepeatedUser.cpf },
 };
 const mockDatabase = new Map<number, IUser>().set(0, {
-  email: expectedUser.email,
-  cpf: expectedUser.cpf,
+  email: mEmailRepeatedUser.email,
+  cpf: mCpfRepeatedUser.cpf,
 } as IUser);
 
 const missingCases: (keyof IUser)[] = [
@@ -59,7 +53,6 @@ const missingCases: (keyof IUser)[] = [
 beforeEach(() => {
   spyRepository.readAll.mockReset();
   spyRepository.create.mockClear();
-  mockDatabase.clear();
   spyRepository.readAll.mockImplementation(() => {
     return mockDatabase;
   });
@@ -68,17 +61,13 @@ beforeEach(() => {
 describe('Route /customer', () => {
   describe('GET /customer', () => {
     it('Should return all customers when reading works correctly', async () => {
-      mockDatabase.set(0, {
-        email: expectedUser.email,
-        cpf: '12345678909',
-      } as IUser);
       spyRepository.readAll.mockImplementation(() => {
         return mockDatabase;
       });
       const res = await request(app).get('/customer');
       expect(res).not.toBeUndefined();
       expect(res.status).toBe(200);
-      expect(res.body.message).toEqual(expectedDatabase);
+      expect(res.body.message).toEqual(expectedListJson);
     });
 
     it('Should return reading error when readAll fails', async () => {
@@ -93,72 +82,65 @@ describe('Route /customer', () => {
   });
 
   describe('POST /customer', () => {
+    spyRepository.readAll.mockImplementation(() => {
+      return mockDatabase;
+    });
     it('Should respond with sanitized user json when creating valid user', async () => {
-      spyRepository.create.mockImplementation(() => mockUser);
-      const res = await request(app).post('/customer').send(mockUser);
+      spyRepository.create.mockImplementation(() => mockValidUser);
+      const res = await request(app).post('/customer').send(mockValidUser);
       expect(res).not.toBeUndefined();
       expect(res.status).toBe(201);
       expect(res.body.message).toEqual(expectedUser);
     });
 
     it('Should respond with error when cpf already exists', async () => {
-      mockDatabase.set(0, {
-        cpf: expectedUser.cpf,
-      } as IUser);
-      spyRepository.readAll.mockImplementation(() => {
-        return mockDatabase;
-      });
-      spyRepository.create.mockImplementation(() => mockUser);
-      const res = await request(app).post('/customer').send(mockUser);
+      spyRepository.create.mockImplementation(() => mCpfRepeatedUser);
+      const res = await request(app).post('/customer').send(mCpfRepeatedUser);
       expect(res).not.toBeUndefined();
       expect(res.status).toBe(422);
       expect(res.body.error).toEqual(
-        `Error: CPF ${expectedUser.cpf} already exists`,
+        `Error: CPF ${mCpfRepeatedUser.cpf} already exists`,
       );
     });
 
     it('Should respond with error when email already exists', async () => {
-      mockDatabase.set(0, {
-        email: expectedUser.email,
-      } as IUser);
-      spyRepository.readAll.mockImplementation(() => {
-        return mockDatabase;
-      });
-      spyRepository.create.mockImplementation(() => mockUser);
-      const res = await request(app).post('/customer').send(mockUser);
+      spyRepository.create.mockImplementation(() => mEmailRepeatedUser);
+      const res = await request(app).post('/customer').send(mEmailRepeatedUser);
       expect(res).not.toBeUndefined();
       expect(res.status).toBe(422);
       expect(res.body.error).toEqual(
-        `Error: Email ${expectedUser.email} already exists`,
+        `Error: Email ${mEmailRepeatedUser.email} already exists`,
       );
     });
 
     it('Should respond with error when user cpf is composed only with one number', async () => {
-      mockUser.cpf = '55555555555';
-      spyRepository.create.mockImplementation(() => mockUser);
-      const res = await request(app).post('/customer').send(mockUser);
-      expect(res).not.toBeUndefined();
-      expect(res.status).toBe(422);
-      expect(res.body.error).toEqual(`Error: CPF ${mockUser.cpf} is invalid`);
-    });
-
-    it('Should respond with error when user cpf is invalid', async () => {
-      mockUser.cpf = '55555555551';
-      spyRepository.create.mockImplementation(() => mockUser);
-      const res = await request(app).post('/customer').send(mockUser);
-      expect(res).not.toBeUndefined();
-      expect(res.status).toBe(422);
-      expect(res.body.error).toEqual(`Error: CPF ${mockUser.cpf} is invalid`);
-    });
-
-    it('Should respond with error when user postal code is invalid', async () => {
-      spyRepository.create.mockImplementation(() => mockUser);
-      jest.spyOn(axios, 'get').mockImplementation(() => Promise.reject());
-      const res = await request(app).post('/customer').send(mockUser);
+      spyRepository.create.mockImplementation(() => mCpfEqualUser);
+      const res = await request(app).post('/customer').send(mCpfEqualUser);
       expect(res).not.toBeUndefined();
       expect(res.status).toBe(422);
       expect(res.body.error).toEqual(
-        `Error: Postal Code ${expectedUser.postal_code} is invalid`,
+        `Error: CPF ${mCpfEqualUser.cpf} is invalid`,
+      );
+    });
+
+    it('Should respond with error when user cpf is invalid', async () => {
+      spyRepository.create.mockImplementation(() => mCpfInvalidUser);
+      const res = await request(app).post('/customer').send(mCpfInvalidUser);
+      expect(res).not.toBeUndefined();
+      expect(res.status).toBe(422);
+      expect(res.body.error).toEqual(
+        `Error: CPF ${mCpfInvalidUser.cpf} is invalid`,
+      );
+    });
+
+    it('Should respond with error when user postal code is invalid', async () => {
+      spyRepository.create.mockImplementation(() => mPCodeInvalidUser);
+      jest.spyOn(axios, 'get').mockImplementation(() => Promise.reject());
+      const res = await request(app).post('/customer').send(mPCodeInvalidUser);
+      expect(res).not.toBeUndefined();
+      expect(res.status).toBe(422);
+      expect(res.body.error).toEqual(
+        `Error: Postal Code ${mPCodeInvalidUser.postal_code} is invalid`,
       );
     });
 
@@ -167,8 +149,8 @@ describe('Route /customer', () => {
       async (firstParam) => {
         jest
           .spyOn(UserRepository.prototype, 'create')
-          .mockImplementation(() => mockUser);
-        (mockUser[firstParam] as unknown) = undefined;
+          .mockImplementation(() => mockValidUser);
+        (mockValidUser[firstParam] as unknown) = undefined;
         const res = await request(app).post('/customer').send();
         expect(res).not.toBeUndefined();
         expect(res.status).toBe(422);
---
+++ src/presentation/routes/mocks/UserRouter.mock.ts
@@ -0,0 +1,44 @@
+import { IUser } from '@interfaces/domain/repository';
+
+export const mockValidUser: IUser = {
+  full_name: 'Teste do Teste',
+  email: 'test@test.com',
+  email_confirmation: 'test@test.com',
+  cpf: '123.456.789-09',
+  cellphone: '(47) 991234567',
+  birthdate: '2000-01-01',
+  email_sms: false,
+  whatsapp: false,
+  country: 'Brazil',
+  city: 'Blumenau',
+  postal_code: '89010-203',
+  address: 'Rua X, 001, Itoupava',
+};
+
+export const mCpfRepeatedUser: IUser = {
+  ...mockValidUser,
+  cpf: '19087282052',
+};
+
+export const mEmailRepeatedUser: IUser = {
+  ...mockValidUser,
+  email: 'repeated@repeated.com',
+  email_confirmation: 'repeated@repeated.com',
+};
+
+export const mCpfEqualUser: IUser = {
+  ...mockValidUser,
+  email: 'equal@equal.com',
+  email_confirmation: 'equal@equal.com',
+  cpf: '11111111111',
+};
+
+export const mCpfInvalidUser: IUser = {
+  ...mockValidUser,
+  cpf: '10897799900',
+};
+
+export const mPCodeInvalidUser: IUser = {
+  ...mockValidUser,
+  postal_code: '01010101',
+};
